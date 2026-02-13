3b88a5750a7c21860532817dbee9e84e44cc2dda
rjordonez <rexjordonez@gmail.com>
2026-02-13 14:37:25 -0800

    Update files

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,6 +20,7 @@ function Navbar() {
           <li><a href="#features">Features</a></li>
           <li><a href="#faq">FAQ</a></li>
         </ul>
+        <button className="mobile-cta-button">Request Demo</button>
       </div>
     </nav>
   );
